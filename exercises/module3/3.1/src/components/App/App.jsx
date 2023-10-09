c69d4ddb1bd80dc00b58bed632113718ea32c13e
Denis Nguyen <Den.nguyen69@gmail.com>
2023-10-09 11:04:19 +0200

    module3 : loading

--- exercises/module3/3.1/src/components/App/App.jsx
+++ exercises/module3/3.1/src/components/App/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
 import Button from 'components/Button/Button'
 import Statistics from 'components/Statistics/Statistics'
+import Loading from 'components/Loading/Loading'
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [loading, setLoading] = useState(true)
 
   const changeCount = (e) => {
     if (e.target.className === "good") setGood(good + 1)
     else if (e.target.className === "neutral") setNeutral(neutral + 1)
     else setBad(bad + 1)
   }
+
+  setTimeout(() => setLoading(false), 3000)
+
+  if (loading) return <Loading />
 
   return (
     <div>
