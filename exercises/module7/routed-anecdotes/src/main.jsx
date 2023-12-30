f3dfb3f69dc8a083b9874345a319fae6093ca7d2
Porridjo <Den.nguyen69@gmail.com>
2023-12-30 01:56:06 +0100

    feat: add ex 7.1

---
+++ exercises/module7/routed-anecdotes/src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppLoader from '/src/components/App/AppLoader'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<AppLoader />)
