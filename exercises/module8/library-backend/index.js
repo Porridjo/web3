4d3ffdd69c3d9d42af69446bee2103230cff8c0a
Porridjo <Den.nguyen69@gmail.com>
2023-12-31 08:06:49 +0100

    feat: add ex 8.7

--- exercises/module8/library-backend/index.js
+++ exercises/module8/library-backend/index.js
@@ -126,6 +126,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -155,6 +159,15 @@ const resolvers = {
         authors = authors.concat({ name: args.author, id: uuidv4() })
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
