63bd09e786414a869369b9e9cfc50d1e9a5385f7
Porridjo <Den.nguyen69@gmail.com>
2024-01-01 05:28:27 +0100

    feat: add ex 8.8 and 8.9

---
+++ exercises/module8/library-frontend/src/utils/queries.js
@@ -0,0 +1,27 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      id
+      name
+      born
+      bookCount
+    }
+  }
+`
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      id
+      title
+      author
+      published
+    }
+  }
+`
+
+export {
+  ALL_AUTHORS,
+  ALL_BOOKS
+}
